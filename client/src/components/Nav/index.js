6fd5f25027b73382166a3d7ffa6e361343393daf
Jared Silver <jrsilver88@gmail.com>
2020-06-25 16:57:34 -0700

    set up api routes and nav component

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="/">
+                <h3 className="text-white">Google Book Search</h3>
+            </a>
+            <div id="nav-bar">
+                <ul className="navbar-nav">
+                    <li className="nav-item" id="home">
+                        <a className="nav-link" href="/">
+                            <button type="button" className="btn btn-info text-white">
+                                Search Books
+                            </button>
+                        </a>
+                    </li>
+                    <li className="nav-item" id="saved">
+                        <a className="nav-link" href="/saved">
+                            <button type="button" className="btn btn-danger text-white">
+                                Saved Books
+                            </button>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
